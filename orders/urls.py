c6923493be6d774dcd84b049d574bacfb63c6ae2
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-07 13:20:21 +0300

    "Added import statements and defined urlpatterns in orders/urls.py"

--- orders/urls.py
+++ orders/urls.py
@@ -1,1 +1,8 @@
 # orders urls
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('checkout/', views.checkout, name='checkout'),
+    path('order_confirmation/<int:order_id>/', views.order_confirmation, name='order_confirmation'),
+]
