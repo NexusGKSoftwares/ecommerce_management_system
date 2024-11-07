c2fecadcf32b34d4f26edafa4d8d768f276dccc4
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-07 13:11:11 +0300

    "Added Profile model with OneToOneField to User and address field"

--- users/models.py
+++ users/models.py
@@ -1,1 +1,7 @@
 # users models
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField()
