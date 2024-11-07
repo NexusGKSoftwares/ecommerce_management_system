4dd489f05e181a4a388c4f88a75daf391bfda240
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-07 13:16:32 +0300

    "Added CartItem model to cart app with fields for product, quantity, and user, including total_price method and string representation."

--- cart/models.py
+++ cart/models.py
@@ -1,1 +1,15 @@
 # cart models
+from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart', null=True, blank=True)
+
+    def total_price(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
