10580d571bdb8ff6e9a9bd5b864975c1f424e366
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-07 13:17:45 +0300

    "Added URL patterns for cart, add to cart, and remove from cart views in cart/urls.py"

--- cart/urls.py
+++ cart/urls.py
@@ -1,1 +1,9 @@
 # cart urls
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.cart, name='cart'),
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+]
