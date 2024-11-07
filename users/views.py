51b1ec5eacd60ba71b1bde6b1dadcfb9ee063ecc
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-07 13:11:53 +0300

    "Added register and login views to users app"

--- users/views.py
+++ users/views.py
@@ -1,1 +1,21 @@
 # users views
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('product_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        # Implement login logic here
+        pass
+    return render(request, 'users/login.html')
