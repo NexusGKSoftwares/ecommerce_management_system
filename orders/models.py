cd4620bb0e982add878aab91309bf3e39687144b
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-07 13:19:09 +0300

    "Added Order model to orders app with fields for user, customer info, total amount, creation time, and status."

--- orders/models.py
+++ orders/models.py
@@ -1,1 +1,18 @@
 # orders models
+from django.db import models
+from products.models import Product
+from users.models import Profile
+from django.contrib.auth.models import User
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.TextField()
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=100, choices=[('Pending', 'Pending'), ('Completed', 'Completed')], default='Pending')
+
+    def __str__(self):
+        return f'Order {self.id} by {self.user.username}'
