b428cbfa4c13398de6aa3b2fe2500b11adc51243
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-07 13:05:21 +0300

    "Updated settings.py: modified TEMPLATES DIRS, added STATICFILES_DIRS and MEDIA_ROOT, and removed comments"

--- ecommerce_management_system/settings.py
+++ ecommerce_management_system/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'ecommerce_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,12 +118,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
